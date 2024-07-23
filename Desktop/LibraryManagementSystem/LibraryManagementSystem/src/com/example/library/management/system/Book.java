42ae883128fb57ce2700106b98776eb739e782c4
styloraushan <raushanmansagar12345@gmail.com>
2024-07-23 20:15:42 +0530

    Initial commit

---
+++ Desktop/LibraryManagementSystem/LibraryManagementSystem/src/com/example/library/management/system/Book.java
@@ -0,0 +1,35 @@
+package com.example.library.management.system;
+
+public class Book {
+    private int bookId;
+    private String title;
+    private String author;
+    private boolean available;
+
+    public Book(int bookId, String title, String author) {
+        this.bookId = bookId;
+        this.title = title;
+        this.author = author;
+        this.available = true; // Initially all books are available
+    }
+
+    public int getBookId() {
+        return bookId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public boolean isAvailable() {
+        return available;
+    }
+
+    public void setAvailable(boolean available) {
+        this.available = available;
+    }
+}
---
+++ Desktop/LibraryManagementSystem/LibraryManagementSystem/src/com/example/library/management/system/LibraryUser.java
@@ -0,0 +1,39 @@
+package com.example.library.management.system;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LibraryUser {
+    private int userId;
+    private String name;
+    private List<Integer> borrowedBooks;
+
+    public LibraryUser(int userId, String name) {
+        this.userId = userId;
+        this.name = name;
+        this.borrowedBooks = new ArrayList<>();
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public List<Integer> getBorrowedBooks() {
+        return borrowedBooks;
+    }
+
+    public void borrowBook(int bookId) {
+        borrowedBooks.add(bookId);
+    }
+
+    public void returnBook(int bookId) {
+        borrowedBooks.remove(Integer.valueOf(bookId));
+    }
+}
